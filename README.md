a0c41211f3e7f2423e3206c29e2d660b16a68e7f
NearSkys <nawerslovengood@gmail.com>
2024-12-31 06:19:19 +0000

    `add api/v1/status` endpoint

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Tap-news-clone
+
 Este é um repositório criado para estudos no curso de javascript do felipe deschamps
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Você sabia que penguins azuis gostam de maçãs vermelhas?</h1>;
+  return <h1>Você sabia que penguins azuis gostam de maçãs vermelhas?</h1>;
 }
 
 export default Home;
